e6ec322b5a185eb9207d3eedf21644285d81cc88
FrexPP <franferreyra1616@gmail.com>
2025-10-23 16:18:55 -0300

    #1: Carga del archivo y problema principal.py

---
+++ Carga del archivo y problema principal.py
@@ -0,0 +1,13 @@
+# Importación de librerías y solucionar problema principal:
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("C:/Users/franf/OneDrive/Desktop/dirty_cafe_sales.csv")
+df.info()
+df
+# Después de examinar el DataFrame noté que tenía valores nulos en 7 de las 8 columnas.
+# También había varios valores como "ERROR" y "UNKNOWN", así que transformé tales valores en nulos.
+df.replace(["ERROR","UNKNOWN","NaN","<NA>"],pd.NA,inplace=True)
+# Elimino filas duplicadas.
+df.drop_duplicates(inplace=True)
